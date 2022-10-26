1f023dadcabbf7b4975cc291442ac3230a02c690
rpadliwinata <rpadliwinata@student.telkomuniversity.ac.id>
2022-10-26 14:18:25 +0700

    initial

--- settings_example.py
+++
@@ -1,9 +0,0 @@
-# JWT
-ACCESS_TOKEN_EXPIRE_MINUTES = 1
-REFRESH_TOKEN_EXPIRE_MINUTES = 1
-ALGORITHM = "encription algorithm"
-JWT_SECRET_KEY = "secret key"
-JWT_REFRESH_SECRET_KEY = "secret key"
-
-# DETA
-PROJECT_KEY = "project key"
