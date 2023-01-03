504b3b4f2cf78fe833203380d249345eaf1892f5
naufalchairurrijal <naufalalifaza@student.telkomuniversity.ac.id>
2023-01-03 23:17:32 +0700

    update pengungsian

--- v2/app/pengungsian.py
+++ v2/app/pengungsian.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, Depends
 from deps import get_current_user
 from v2.schemas.response import ResponseFormat
 from db import db_pengungsian
+from v2.schemas.pengungsian import PengungsianIn
 
 
 router = APIRouter(
@@ -19,3 +20,15 @@ async def getAllPengungsian():
         'success': True,
         'message': 'Berhasil menampilkan data pengungsian',
         'data':{'list_pengungsian':pengungsian}}
+
+@router.patch('/update', response_model= ResponseFormat)
+async def update_date(key: str, pengungsian: PengungsianIn):
+  res = db_pengungsian.fetch({"key": key})
+
+  db_pengungsian.update(pengungsian.dict(), res.items[0]['key'])
+
+  return {
+    "message": "Berhasil update data!",
+    'status': 200, 
+    'success': True,
+    'data':{'list_pengungsian':pengungsian}}
