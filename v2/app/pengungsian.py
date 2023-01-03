7b9cfa5e9b42dccc624439f8a7cd1e57a520dff1
amaliayunaa <malyayn@gmail.com>
2023-01-03 21:35:54 +0700

    buat fungsi delete

--- v2/app/pengungsian.py
+++ v2/app/pengungsian.py
@@ -11,17 +11,29 @@ from v2.schemas.pengungsian import PengungsianGet, PengungsianIn, PengungsianDB,
 from v2.schemas.user import UserOut
 from db import db_profil, db_pengelola, db_pengungsian, db_alamat, db_user
 from drive import drive_pengungsian
+from deta import Deta
+from fastapi import FastAPI
 
+deta = Deta("c0xyaz4k_HiLWrZZpCCXESBZZXe6LAsMcSp3dnx1f")
+db = deta.Base("db_pengungsian")
+app = FastAPI(title="Sipenca")
 
 router = APIRouter(
     prefix="/pengungsian",
     tags=["pengungsian"],
-    dependencies=[Depends(get_current_user)],
+    # dependencies=[Depends(get_current_user)],
 )
 
+# @router.get('/')
+# async def contoh():
+#     return {'message': 'berhasil'}
 
-@router.get('/')
-async def contoh():
-    return {'message': 'berhasil'}
 
+@router.delete('/')
+async def delete_data(key: str):
+    res = db.fetch({"key": key})
 
+    db.delete(res.items[0]["key"])
+    return {
+        "message": "Berhasil hapus data!"
+    }
