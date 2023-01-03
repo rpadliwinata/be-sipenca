cd80be439d8db7d11e9b9ec79fa37204b124dde3
amaliayunaa <malyayn@gmail.com>
2023-01-03 22:10:30 +0700

    menambah response format

--- v2/app/pengungsian.py
+++ v2/app/pengungsian.py
@@ -1,18 +1,14 @@
-from uuid import uuid4, UUID
-from datetime import datetime
-from typing import List
-from fastapi import APIRouter, Depends, HTTPException, status, UploadFile
+
+from fastapi import APIRouter, Depends
 from fastapi.responses import StreamingResponse
 from pydantic import ValidationError
 from deps import get_current_user
-from v2.schemas.alamat import AlamatDB
-from v2.schemas.pengelola import PengelolaDB, PengelolaAdd, PengelolaOut
-from v2.schemas.pengungsian import PengungsianGet, PengungsianIn, PengungsianDB, PengungsianOut
-from v2.schemas.user import UserOut
-from db import db_profil, db_pengelola, db_pengungsian, db_alamat, db_user
-from drive import drive_pengungsian
+from db import db_pengungsian
 from deta import Deta
 from fastapi import FastAPI
+from v2.schemas import pengungsian
+from v2.schemas.response import ResponseFormat
+from v2.schemas.pengungsian import PengungsianIn, PengungsianOut
 
 deta = Deta("c0xyaz4k_HiLWrZZpCCXESBZZXe6LAsMcSp3dnx1f")
 db = deta.Base("db_pengungsian")
@@ -21,19 +17,17 @@ app = FastAPI(title="Sipenca")
 router = APIRouter(
     prefix="/pengungsian",
     tags=["pengungsian"],
-    # dependencies=[Depends(get_current_user)],
+    dependencies=[Depends(get_current_user)],
 )
 
-# @router.get('/')
-# async def contoh():
-#     return {'message': 'berhasil'}
-
-
-@router.delete('/')
+@router.delete("/", response_model=ResponseFormat)
 async def delete_data(key: str):
-    res = db.fetch({"key": key})
+    res = db.fetch({'key': key})
 
     db.delete(res.items[0]["key"])
     return {
-        "message": "Berhasil hapus data!"
+        'message': "Berhasil hapus data pengungsian!",
+        'status' : 200,
+        'success' :True,
+        'data': {'list_pengungsian':pengungsian}
     }
