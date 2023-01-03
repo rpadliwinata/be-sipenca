40f53bce7370866d8f2e73eec02c8091408420bd
amaliayunaa <malyayn@gmail.com>
2023-01-03 22:41:03 +0700

    merubah fungsi hapus pengungsian

--- v2/app/pengungsian.py
+++ v2/app/pengungsian.py
@@ -1,17 +1,10 @@
 
 from fastapi import APIRouter, Depends
 from deps import get_current_user
-from deta import Deta
-from fastapi import FastAPI
-from v2.schemas import pengungsian
 from v2.schemas.response import ResponseFormat
-from deta import Deta
 from fastapi import APIRouter, Depends
 from deps import get_current_user
-
-deta = Deta("c0xyaz4k_HiLWrZZpCCXESBZZXe6LAsMcSp3dnx1f")
-db = deta.Base("db_pengungsian")
-app = FastAPI(title="Sipenca")
+from db import db_pengungsian
 
 router = APIRouter(
     prefix="/pengungsian",
@@ -21,14 +14,14 @@ router = APIRouter(
 
 @router.delete("/", response_model=ResponseFormat)
 async def delete_data(key: str):
-    res = db.fetch({'key': key})
+    res = db_pengungsian.fetch({'key': key})
 
-    db.delete(res.items[0]["key"])
+    db_pengungsian.delete(res.items[0]["key"])
     return {
         'message': "Berhasil hapus data pengungsian!",
         'status' : 200,
         'success' :True,
-        'data': {'list_pengungsian':pengungsian}
+        'data': {'list_pengungsian':res.items[0]}
     }
 
 
